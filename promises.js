4a14808f3e209d5a614ab172b4dc3e58152b73ef
AbhishekBhatti8 <abhishekbhatti89.com>
2024-07-14 20:47:54 +0530

    promises, fetch completed

--- promises.js
+++ promises.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 const promiseOne = new Promise(function (resolve, reject) {
   //Do an async Task
   setTimeout(() => {
@@ -31,3 +33,60 @@ const promiseThree = new Promise(function (resolve, reject) {
 promiseThree.then(function (user) {
     console.log(user)
 });
+
+const promiseFour = new Promise (function(resolve, reject){
+    setTimeout(() => {
+        let error = false;
+
+        if (!error){
+            resolve({status: "Saved", timeStamp: Date.now()})
+        } else {
+            reject("Error: Data not saved")
+        }
+    }, 1000);
+})
+
+promiseFour.then((status)=>{
+    console.log(status);
+    return status.status
+}).then(function(status){
+    console.log(status);
+}).catch(function (error){
+    console.log(error)
+}).finally(function(){
+    console.log("promise has been resolved or rejected");
+})
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+
+    if (!error) {
+      resolve({ userName: "javaScript", Pass: "1234" });
+    } else {
+      reject("Error: JS file not found");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(function (res) {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
